d636c6308fc7a55beb9f0074f92f18024d64439b
Ebenezer Belay <ebenezerg.belay@gmail.com>
2021-12-29 20:13:55 -0500

    Home Page Update

---
+++ home.js
@@ -0,0 +1,87 @@
+let carts = document.querySelectorAll(".addtocart");
+let products = [
+  {
+    name: "Red dress",
+    price: 1450,
+    inCart: 0,
+  },
+  {
+    name: "White dress",
+    price: 780,
+    inCart: 0,
+  },
+  {
+    name: "Black dress",
+    price: 500,
+    inCart: 0,
+  },
+  {
+    name: "Yellow dress",
+    price: 450,
+    inCart: 0,
+  },
+];
+for (let i = 0; i < carts.length; i++) {
+  carts[i].addEventListener("click", () => {
+    cartNumbers(products[i]);
+    totalCost(products[i]);
+  });
+}
+
+function onLoadCartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
+  if (productNumbers) {
+    document.querySelector(".counter").textContent = productNumbers;
+  }
+}
+
+function cartNumbers(product) {
+  let productNumbers = localStorage.getItem("cartNumbers");
+
+  productNumbers = parseInt(productNumbers);
+
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    document.querySelector(".counter").textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    document.querySelector(".counter").textContent = 1;
+  }
+
+  setItems(product);
+}
+
+function setItems(product) {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems != null) {
+    if (cartItems[product.name] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.name]: product,
+      };
+    }
+    cartItems[product.name].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.name]: product,
+    };
+  }
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+function totalCost(product) {
+  let cartCost = localStorage.getItem("totalCost");
+
+  if (cartCost != null) {
+    cartCost = parseInt(cartCost);
+    localStorage.setItem("totalCost", cartCost + product.price);
+  } else {
+    localStorage.setItem("totalCost", product.price);
+  }
+}
+
+onLoadCartNumbers();
