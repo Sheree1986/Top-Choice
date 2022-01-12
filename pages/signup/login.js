6a96097ec9b2a97dac10c2bd69eadfa36968bdff
Sheree <sheree.edmund@gmail.com>
2022-01-11 21:37:44 -0500

    update bestseller

--- pages/signup/login.js
+++ pages/signup/login.js
@@ -11,7 +11,7 @@ const signUp = e => {
             data.fname.toLowerCase() == fname.toLowerCase() && 
             data.lname.toLowerCase() == lname.toLowerCase()
         );
-
+        console.log("testing1");
     if(!exist){
         formData.push({ fname, lname, email, pwd });
         localStorage.setItem('formData', JSON.stringify(formData));
@@ -24,16 +24,16 @@ const signUp = e => {
     }
     e.preventDefault();
 }
-
-function signIn(e) {
+console.log("testing2");
+function signIn() {
     let email = document.getElementById('email').value, pwd = document.getElementById('pwd').value;
-    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+    // let formData = JSON.parse(localStorage.getItem('formData')) || [];
     // let exist = formData.length && 
     // JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() == email && data.pwd.toLowerCase() == pwd);
     
-    
-    if (email === "user@gmail.com" && password === "user") {
-        location.href = "../checkout.html";
+    console.log("testing3");
+    if (email === "user@gmail.com" && pwd === "user") {
+        window.location.href = "../checkout.html";
      
     
   
